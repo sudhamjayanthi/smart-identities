7e9209effa3b25566056a14bf4b53a051d163ebf
Sudham Jayanthi <workwithsudham@gmail.com>
2022-05-29 13:21:15 +0530

    chore : clean up 🧹

--- backend/arguments.js
+++ backend/arguments.js
@@ -1,4 +1,7 @@
 module.exports = [
-  ["", ""],
+  [
+    "0xeCf76CC823Fd31C1E473cfD3D2ca04DD2f338Fe3",
+    "0x799c7ab04cd4643dccea669fe08e15004630aa7c",
+  ],
   [50, 50],
 ];
--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -2,36 +2,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // const [s1, s2] = await ethers.getSigners()
-
   const identityFactory = await ethers.getContractFactory("Identities");
-  // const identityFactory = await ethers.getContractFactory("IdentityFactory");
   const identity = await identityFactory.deploy();
-  // const identity = await identityFactory.deploy([s1.address,s2.address],[50,50]);
   await identity.deployed();
 
   console.log("Identities Generator deployed to:", identity.address);
-  // console.log("Identity deployed to:", identity.address);
-
-  // console.log("Contract Balance : ",  ethers.utils.formatEther(await ethers.provider.getBalance(identity.address)));
-
-  // console.log("funding contract..")
-  // await identity.fallback({ value: ethers.utils.parseEther("0.1") });
-  // console.log("Contract Balance : ", ethers.utils.formatEther(await ethers.provider.getBalance(identity.address)));
-
-  // // const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, ethers.getDefaultProvider());
-  // const signerAddress = s1.address;
-  // // const signerAddress = await signer.getAddress();
-
-  // console.log("Initial wallet bal: ", ethers.utils.formatEther(await ethers.provider.getBalance(signerAddress)));
-
-  // console.log("withdrawing..")
-
-  // const data = await identity.connect(s1).callStatic.withdraw();
-  // console.log(data.map(n => ethers.utils.formatEther(n)));
-
-  // console.log("Wallet bal: ", ethers.utils.formatEther(await ethers.provider.getBalance(signerAddress)));
-  // console.log("Contract Balance : ", ethers.utils.formatEther(await ethers.provider.getBalance(identity.address)));
 }
 
 const runMain = () => {
