e8930fde7032e2b1f13e835c8568c3d3ed97eaf0
Sudham Jayanthi <workwithsudham@gmail.com>
2022-06-07 11:26:07 +0530

    refactor : owners moved into a seperate component

---
+++ frontend/src/components/Owners.tsx
@@ -0,0 +1,14 @@
+import Owner from "./Owner"
+
+const Owners = ({ owners, identityConfig }) => {
+    return (<div>
+        <h2 className="subheading mb-5">Owners</h2>
+        <div className="w-[65%] flex rounded overflow-hidden shadow-[9px_9px_45px_-17px_rgba(0,0,0,0.75)]">
+            {owners?.map((address, idx) => {
+                return <Owner address={address} key={idx} idx={idx} identityConfig={identityConfig} />
+            })}
+        </div>
+    </div>)
+}
+
+export default Owners
--- frontend/src/pages/identity/[address].tsx
+++ frontend/src/pages/identity/[address].tsx
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
 import { useAccount, useBalance, useContractRead } from "wagmi";
-import Owner from "@components/Owner";
-import ERC20s from "@/components/ERC20s";
+
+import Owners from "@components/Owners";
 import NFTs from "@components/NFTs";
+import ERC20s from "@components/ERC20s";
 import QuickActions from "@components/QuickActions";
 
 import IdentityABI from "@/utils/Identity.json"
@@ -28,21 +29,18 @@ const Identity = () => {
     return (
         <div className="flex flex-1">
             <div className="w-1/6 flex flex-col justify-start items-center p-10 pt-20">
+                {/* avatar */}
                 <span style={{ backgroundColor: color }} className="text-4xl grid place-content-center select-none h-20 w-20 rounded-full">{emoji}</span>
+                {/* balance */}
                 <span className="text-xl mt-4 font-bold">{bal?.formatted} {bal?.symbol}</span>
             </div>
+            
+            {/* identity details */}
             <div className="flex flex-col flex-1 p-10 pt-20 gap-10">
                 <h1 className="text-2xl font-black cursor-pointer" onClick={() => copyToClipboard(identityAddress)}>
                     {identityAddress ? identityAddress : "Loading..."}
                 </h1>
-                <div>
-                    <h2 className="subheading mb-5">Owners</h2>
-                    <div className="w-[65%] flex rounded overflow-hidden shadow-[9px_9px_45px_-17px_rgba(0,0,0,0.75)]">
-                        {owners?.map((address, idx) => {
-                            return <Owner address={address} key={idx} idx={idx} identityConfig={identityConfig} />
-                        })}
-                    </div>
-                </div>
+                <Owners identityConfig={identityConfig} owners={owners} />
                 <NFTs identityConfig={identityConfig} isOwner={isOwner} />
                 <ERC20s identityConfig={identityConfig} isOwner={isOwner} />
                 <QuickActions identityConfig={identityConfig} isOwner={isOwner} />
