5aaf8ddd8c384336a1bf9161c49bf04a6992e9bd
Sudham Jayanthi <workwithsudham@gmail.com>
2022-08-14 20:27:02 +0530

    landing done!

---
+++ landing/next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
+};
